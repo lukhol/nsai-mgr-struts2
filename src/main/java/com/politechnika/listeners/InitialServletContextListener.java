5f8c0db2aa55d87111fc5cf8a0fa87f5cf25b214
lukhol <lukasz.holdrowicz@gmail.com>
2017-11-06 17:09:33 +0100

    Added context listener which is adding roles to the database on the server start.

---
+++ src/main/java/com/politechnika/listeners/InitialServletContextListener.java
@@ -0,0 +1,48 @@
+package com.politechnika.listeners;
+
+import java.util.List;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.politechnika.models.UserRole;
+import com.politechnika.services.UserService;
+
+@WebListener
+public class InitialServletContextListener implements ServletContextListener {
+
+	@Autowired
+	UserService userService;
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		System.out.println("Started adding role");
+		
+		//Enable spring autowired in servlet context listener:
+		WebApplicationContextUtils
+        	.getRequiredWebApplicationContext(arg0.getServletContext())
+        	.getAutowireCapableBeanFactory()
+        	.autowireBean(this);
+		
+		List<UserRole> userRoles = userService.listUserRole();
+		if(userRoles == null || userRoles.size() == 0) {
+			UserRole roleAdmin = new UserRole();
+			roleAdmin.setRole("ROLE_ADMIN");
+			userService.addRole(roleAdmin);
+			
+			UserRole roleUser = new UserRole();
+			roleUser.setRole("ROLE_USER");
+			userService.addRole(roleUser);
+		}
+	}
+
+}
